bf0a07d6fda9646e94b7c316e85520ef55215641
Dantol91 <44117971+Dantol91@users.noreply.github.com>
2018-11-15 23:22:35 +0100

    Cambios en PopulateDatabase.xml de Acme Handy Worker
    Añadidos ultimos cambios en el archivo PopulateDatabase.xml

--- Acme Handy Worker/src/main/java/domain/Application.java
+++ Acme Handy Worker/src/main/java/domain/Application.java
@@ -7,6 +7,8 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -37,6 +39,7 @@ public class Application extends DomainEntity {
 
 	@Past
 	@NotNull
+	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	public Date getRegisterMoment() {
 		return this.registerMoment;
